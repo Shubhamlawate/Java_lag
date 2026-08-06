d02ae4443fe5d431d836c8017aae4c94e8cc9099
Shubham <lavateshubham11@gmail.com>
2026-08-07 01:03:40 +0530

    Add files via upload

---
+++ loop.java
@@ -0,0 +1,60 @@
+
+import java.util.*;
+public class loop {
+//    public static void main(String[] args){
+//         String  x = "*" ;
+//        for(int i=0;i<=5;i++){
+//            for(int j =0;j<=5;j++) {
+//                System.out.println(x                                                                                                        );
+//            }
+//        }
+//    }
+
+
+//    public static void main(String[] args){
+//        for(int i=0;i<=10;i++){
+//            if(i%2==0) {
+//                System.out.println(i);
+//            }
+//        }
+//    }
+
+//    public static  void main(String args[]){
+//        int i=0;
+//        while ( i<=10){
+//            System.out.println(i);
+//            i++;
+//        }
+//    }
+
+//    public static void main(String args[]){
+//        int i=0;
+//        do{
+//            System.out.println(i);
+//            i++;
+//        }while (i<=10);
+//    }
+    // sum of n natura numbers
+//    public static void main(String args[]){
+//        Scanner sc = new Scanner(System.in);
+//        int n= sc.nextInt();
+//        int i = 0;
+//        int sum =0;
+//        for(i=1;i<=n;i++){
+////            System.out.println();
+//             sum +=i  ;
+//        }
+//        System.out.println(sum);
+//
+//    }
+    //print table
+//    public static void main(String args[]){
+//        Scanner sc =new Scanner(System.in);
+//        int n =sc.nextInt();
+//        for(int i =0;i<=10;i++){
+//            System.out.println(n*i);
+//
+//        }
+//    }
+
+}
