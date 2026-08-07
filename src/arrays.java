0cd898a4fa143f4c051d499c1cb408fc7805461b
Shubham Lavate <lavateshubham11@gmail.com>
2026-08-08 00:06:01 +0530

    init commit

--- src/arrays.java
+++ src/arrays.java
@@ -37,31 +37,61 @@ public class arrays {
 
     // TAKE INPUT USERS AND THAT ARRAY SERCH X NUMBER AND ORINT INDEX
 
-    public static void main(String[] args){
+//    public static void main(String[] args){
+//
+//        Scanner sc = new Scanner(System.in);
+//
+//        int size =sc.nextInt();
+//
+//        int ages [] =new int[size];
+//
+//        for (int i= 0;i<size;i++){
+//            ages[i]=sc.nextInt();
+//        }
+//        System.out.print("Enter the x :");
+//        int x =sc.nextInt();
+//        for(int i=0;i<ages.length;i++){
+//
+//            if(ages[i]==x){
+//                System.out.println("index of x:"+i);
+//            }
+//
+//        }
+//
+//        for(int i=0;i<size;i++){
+//            System.out.print(ages[i]+"  ");
+//        }
+//    }
 
-        Scanner sc = new Scanner(System.in);
 
-        int size =sc.nextInt();
+    //Binary algorithem
+     static int binarysearch(int[] arr,int target){
+         int low =0;
+         int high=arr.length;
+         while (low<=high){
+             int mid =low +(high-low)/2;
+             if(arr[mid]==target){
+                 return mid;
+             }
+             if(target<arr[mid]){
+                 high=mid-1;
+             }else {
+                 low=mid+1;
+             }
+         }
+         return -1;
+     }
 
-        int ages [] =new int[size];
+     public static void main(String[] args){
+         int[]arr={10,20,30,40,50,60,70};
+         int target=50;
+        int serach= binarysearch(arr,target);
+         if(serach!=-1){
+             System.out.println(serach);
 
-        for (int i= 0;i<size;i++){
-            ages[i]=sc.nextInt();
-        }
-        System.out.print("Enter the x :");
-        int x =sc.nextInt();
-        for(int i=0;i<ages.length;i++){
-
-            if(ages[i]==x){
-                System.out.println("index of x:"+i);
-            }
-
-        }
-
-        for(int i=0;i<size;i++){
-            System.out.print(ages[i]+"  ");
-        }
-    }
-
+         }else {
+             System.out.println("not found");
+         }
+     }
 
 }
